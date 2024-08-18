0905be52c85c21d9a4d05130015905bb15f653aa
Vedant Kashyap <varun.567.kashyap@gmail.com>
2024-08-18 18:44:11 +0530

    home

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,23 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home";
 import EditorPage from "./Pages/EditorPage";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <>
+      <div>
+        <Toaster
+          position="top-center"
+          toastOptions={{
+            success: {
+              theme: {
+                primary: "#4aed88",
+              },
+            },
+          }}
+        ></Toaster>
+      </div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,22 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+  const [roomId, setRoomId] = useState("");
+  const [username, setUsername] = useState("");
+
+  const creatNewRoom = (e) => {
+    e.preventDefault();
+    const id = uuidv4();
+    // console.log(id);
+    setRoomId(id);
+    toast.success("Created new room");
+  };
+
+  const joinRoom = () => {
+    if (!roomId || !username) {
+      toast.error("ROOM ID & username is required");
+      return;
+    }
+
+    //redirect
+    navigate(`/editor/:${roomId}`, {
+      state: {
+        username,
+      },
+    });
+  };
+
+  const handleInputEnter = (e) => {
+    // console.log(e.code);
+    if (e.code === "Enter") {
+      joinRoom();
+    }
+  };
+
   return (
     <div className="homePageWrapper">
       <div className="formWrapper">
         <img src="/assets/viCode.png" alt="" className="logo" />
         <h4 className="mainLabel">Paste invitation ROOM ID</h4>
         <div className="inputGroup">
-          <input type="text" className="inputBox" placeholder="ENTER ROOM ID" />
-          <input type="text" className="inputBox" placeholder="USERNAME" />
-          <button className="btn joinbtn">Join</button>
+          <input
+            type="text"
+            value={roomId}
+            onChange={(e) => setRoomId(e.target.value)}
+            className="inputBox"
+            placeholder="ENTER ROOM ID"
+            onKeyUp={handleInputEnter}
+          />
+          <input
+            type="text"
+            className="inputBox"
+            placeholder="USERNAME"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            onKeyUp={handleInputEnter}
+          />
+          <button className="btn joinbtn" onClick={joinRoom}>
+            Join
+          </button>
         </div>
-        <span className="creatInfo">
-          If you don;t have invite then create &nbsp;
-          <a href="" className="createNewBtn">
-            new Room
-          </a>
-        </span>
+        <div className="creatInfo">
+          <span>
+            If you don't have invite then create &nbsp;
+            <button onClick={creatNewRoom} className="createNewBtn">
+              Create New Room
+            </button>
+          </span>
+        </div>
       </div>
       <footer>
         <h4>
